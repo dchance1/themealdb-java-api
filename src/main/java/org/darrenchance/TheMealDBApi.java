67252517264e7e6336ac94cac9bc2bfe14ff4938
Darren Chance <dchance1@mail.valenciacollege.edu>
2024-06-27 15:48:34 -0400

    Created class for api access to meal database. Class will allow user to interface with api website programmatically.

---
+++ src/main/java/org/darrenchance/TheMealDBApi.java
@@ -0,0 +1,13 @@
+package org.darrenchance;
+
+/**
+ * Darren Chance<br>
+ * June 27, 2024<br>
+ * <p>
+ * <p>
+ * The class {@code TheMealDBAP} is a java interface for getting recipes for from the https://www.themealdb.com/api.php
+ * allowing user to access information from the website programatically.
+ */
+public class TheMealDBApi {
+
+}
