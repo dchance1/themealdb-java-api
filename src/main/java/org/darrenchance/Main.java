12f9ad12781146289ed55048fd2a50b56286e6dc
Darren Chance <dchance1@mail.valenciacollege.edu>
2024-07-04 11:14:25 -0400

    Refactoring

--- src/main/java/org/darrenchance/Main.java
+++ src/main/java/org/darrenchance/Main.java
@@ -1,23 +1,16 @@
 package org.darrenchance;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
-import java.io.ObjectStreamException;
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class Main {
-
-
     public static void main(String[] args) {
         TheMealDBApi api = new TheMealDBApi();
-        String json = api.getMealsByMainIngredient("beef");
+        String json = api.getMealsByRandom();
 
-        ArrayList<Recipe> recipes = Recipe.importRecipes(json, Recipe.Api.THE_MEAL_DB );
+        ArrayList<Recipe> recipes = Recipe.importRecipes(json, Recipe.Api.THE_MEAL_DB);
+        if (recipes == null) {
+            System.exit(0);
+        }
 
         int i = 0;
         for (Recipe recipe : recipes) {
@@ -29,11 +22,9 @@ public class Main {
             System.out.println();
             System.out.println("Ingredients:");
             ArrayList<String> ingredients = recipe.getIngredients();
-            for (String ingredient: ingredients){
+            for (String ingredient : ingredients) {
                 System.out.println(ingredient);
             }
-
-            //System.out.println(json2);
         }
     }
 }
